bea5cf6e323bbad1739b5123c4d1556f8649442b
Satya B <bvvsnc3@gmail.com>
2021-08-03 08:09:19 +0530

    team page css added

--- view/src/components/MatchDetailCard.js
+++ view/src/components/MatchDetailCard.js
@@ -1,20 +1,31 @@
-import {React} from 'react';
-import {  Link } from 'react-router-dom';
-
-export const MatchDetailCard = ({match,teamName})=> {
-  if( !match ) return null;
+import { React } from 'react';
+import { Link } from 'react-router-dom';
+import './MatchDetailCard.scss';
+export const MatchDetailCard = ({ match, teamName }) => {
+  if (!match) return null;
   const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
   const otherTeamRoute = `/teams/${otherTeam}`;
+  const isMatchWon = teamName === match.matchWinner;
   return (
-    <div className="MatchDetailCard">
-        
-        <h3>Latest Matches</h3>
-        <h1> vs <Link to={otherTeamRoute}>{otherTeam}</Link></h1>
-        <h2> vs {match.date}</h2>
-        <h3> vs {match.venue}</h3>
-        <h3> vs {match.matchWinner} won by {match.resultMargin} {match.result}</h3>
+    <div className={ isMatchWon ?'MatchDetailCard won-card' : 'MatchDetailCard lost-card'}>
+      <div>
+        <span className="vs">vs</span>
+        <h1> <Link to={otherTeamRoute}>{otherTeam}</Link></h1>
+        <h2 className="match-date"> {match.date}</h2>
+        <h3 className="match-venue">at {match.venue}</h3>
+        <h3 className="match-result"> {match.matchWinner} won by {match.resultMargin} {match.result} wickets</h3>
+      </div>
+      <div className="additional-details">
+        <h3>First Innings</h3>
+        <p>{match.team1}</p>
+        <h3>Second Innings</h3>
+        <p>{match.team2}</p>
+        <h3>Man of the Match</h3>
+        <p>{match.playerOfMatch}</p>
+        <h3>Umpires</h3>
+        <p>{match.umpire1}, {match.umpire2}</p>
+      </div>
     </div>
   );
 }
 
- 
--- view/src/pages/TeamPage.js
+++ view/src/pages/TeamPage.js
@@ -2,11 +2,13 @@ import { React, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
+import './TeamPage.scss';
+import { PieChart } from 'react-minimal-pie-chart';
 
 export const TeamPage = () => {
 
   const [team, setTeam] = useState({ matches: [] });
-  const {teamName } = useParams();
+  const { teamName } = useParams();
   console.log(teamName);
   useEffect(
     () => {
@@ -25,11 +27,30 @@ export const TeamPage = () => {
   }
   return (
     <div className="TeamPage">
-      <h1>{team.teamName}</h1>
-      <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      <div className="team-name-section">
+        <h1 className="team-name">{team.teamName}</h1>
+      </div>
+      <div className="win-loss-section">Win / Lossess
+        <PieChart
+          data={[
+            { title: 'Losses', value: team.totalMatches-team.totalWins, color: '#e15454' },
+            { title: 'Wins', value: team.totalWins, color: '#4da375' }
+            
+            
+          ]}
+        />
+      </div>
+      <div class="match-detail-setion">
+        <h3>Latest Matches</h3>
+        <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      </div>
       {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match} ></MatchSmallCard>)}
-
+      <div className="more-link">
+        <a href="#"> More > </a>
+      </div>
     </div>
   );
 }
 
+
+ 
