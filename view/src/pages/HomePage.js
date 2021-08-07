428ac3455e24174cfc87d7c8c296fdc13b0db4e5
Satya B <bvvsnc3@gmail.com>
2021-08-07 12:29:50 +0530

    finall

--- view/src/pages/HomePage.js
+++ view/src/pages/HomePage.js
@@ -10,7 +10,7 @@ export const HomePage = () => {
     useEffect(
         () => {
             const fetchAllTeams = async () => {
-                const response = await fetch(`/team`)
+                const response = await fetch(`${process.env.REACT_APP_API_ROOT_URL}/team`)
                 const data = await response.json();
                 setTeams(data);
                 //console.log(data);
